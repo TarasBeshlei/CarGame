956f225c16268cc4683f2fda34f725468f502d43
Taras Beshlei <beshleytaras@gmail.com>
2019-11-11 18:12:47 +0200

    Fixed1

--- LabCar/Assets/CarMovement.cs
+++ LabCar/Assets/CarMovement.cs
@@ -1,68 +1,80 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class CarMovement : MonoBehaviour
+public partial class CarMovement : MonoBehaviour
 {
+    private float moveSpeed = 3f;
+    private float turnSpeed = 40f;
+    private float jumpHeight = 3f;
+    private float reverseRotationDirection = -40f;
+    private float normalRotationDirection = 40f;
+    private Rigidbody rbd;
+    private bool isGrounded;
     
-    public float range = 5f;
-    public float moveSpeed = 3f;
-    public float turnSpeed = 40f;
-    public float jump = 8f;
-    public GameObject obj;
+    
     // Start is called before the first frame update
     void Start()
     {
         
+       // CarJeep = this.gameObject.GetComponentsInParent();
+       rbd = GetComponent<Rigidbody>();
+       rbd.freezeRotation = true;
+       
+    }
+    
+    void  OnCollisionEnter()
+    {
+        isGrounded = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            moveSpeed = 6f;
-        }
-        else
-        {
-            moveSpeed = 3f;
-        }
-        
+        //MoveObject(KeyCode.UpArrow, moveSpeed);
+        //MoveObject(KeyCode.DownArrow, -moveSpeed);
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            obj.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-            
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                obj.transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
-            }
-        
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                obj.transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-            }
+            print("up");
+            rbd.AddForce(Vector3.forward * 5f * Time.deltaTime * 10, ForceMode.Acceleration);
         }
+        RotateObject(KeyCode.LeftArrow, Vector3.down, turnSpeed);
+        RotateObject(KeyCode.RightArrow, Vector3.up, turnSpeed);
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            
+            rbd.useGravity = true;
+           
+            
+            isGrounded = false;
+        }
+
+    }
+
+    private void RotateObject(KeyCode bindKey, Vector3 rotationDiraction, float speed)
+    {
+        if (Input.GetKey(bindKey))
+        {
+           
+            transform.Rotate(rotationDiraction * speed * Time.deltaTime);
+        }
+    }
+
+    private void MoveObject(KeyCode bindKey, float diractionAndSpeed)
+    {
         
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            obj.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
-            
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                obj.transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
-            }
-        
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                obj.transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-            }
-        }
-
-        if (Input.GetKey(KeyCode.Space))
+            turnSpeed = reverseRotationDirection;
+        } else 
         {
-            obj.transform.Translate(Vector3.up * jump * Time.deltaTime);
+            turnSpeed = normalRotationDirection;
+        }
+        
+        if (Input.GetKey(bindKey))
+        {
+            rbd.velocity = transform.forward * diractionAndSpeed;
         }
 
-        
     }
 }
---
+++ LabCar/Assets/TestScropt.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScropt : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool isGrounded = true;
+    private float distToGround = 0.4f;
+    private float rotatingSpeed = 70f;
+    private int speed = 800;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Move(KeyCode.UpArrow, speed);
+        Move(KeyCode.DownArrow, -speed);
+        Rotate(KeyCode.RightArrow, Vector3.up);
+        Rotate(KeyCode.LeftArrow, Vector3.down);
+        Jump(200);
+    }
+
+    private void Jump(int jumpHeight)
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, Vector3.down, distToGround))
+        {
+            rb.AddForce(transform.up * jumpHeight * Time.deltaTime, ForceMode.Impulse);
+        }
+    }
+
+    private void Rotate(KeyCode bindKey, Vector3 rotateDirection)
+    {
+        if (Input.GetKey(bindKey))
+        {
+            transform.Rotate(rotateDirection * rotatingSpeed * Time.deltaTime);
+        }
+    }
+
+    private void Move(KeyCode bindKey, int speedAndDirection)
+    {
+        if (Input.GetKey(bindKey))
+        {
+            rb.AddForce(transform.forward * speedAndDirection * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotatingSpeed = -70f;
+        }
+        else
+        {
+            rotatingSpeed = 70f;
+        }
+    }
+}
