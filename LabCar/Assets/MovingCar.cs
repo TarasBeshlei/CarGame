be1f73d2446bf642608d14c49aba59d00e3fb264
Taras Beshlei <beshleytaras@gmail.com>
2019-11-12 16:23:34 +0200

    CodeStyleAndWaterFix

--- LabCar/Assets/MovingCar.cs
+++ LabCar/Assets/MovingCar.cs
@@ -1,14 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScropt : MonoBehaviour
+public class MovingCar : MonoBehaviour
 {
     private Rigidbody rb;
     private bool isGrounded = true;
-    private float distToGround = 0.4f;
-    private float rotatingSpeed = 70f;
+    private bool isOnWater;
+    private float distanceToGround = 0.4f;
+    private float rotatingSpeed;
+    private int jumpHeight = 300;
+    private float forwardRotating = 70f;
+    private float reverseRotating = -70f;
     private int speed = 800;
+    
     
     // Start is called before the first frame update
     void Start()
@@ -25,12 +31,12 @@ public class TestScropt : MonoBehaviour
         Move(KeyCode.DownArrow, -speed);
         Rotate(KeyCode.RightArrow, Vector3.up);
         Rotate(KeyCode.LeftArrow, Vector3.down);
-        Jump(200);
+        Jump(jumpHeight);
     }
 
     private void Jump(int jumpHeight)
     {
-        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, Vector3.down, distToGround))
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, Vector3.down, distanceToGround))
         {
             rb.AddForce(transform.up * jumpHeight * Time.deltaTime, ForceMode.Impulse);
         }
@@ -38,26 +44,44 @@ public class TestScropt : MonoBehaviour
 
     private void Rotate(KeyCode bindKey, Vector3 rotateDirection)
     {
-        if (Input.GetKey(bindKey))
+        if (Input.GetKey(bindKey) && !isOnWater)
         {
             transform.Rotate(rotateDirection * rotatingSpeed * Time.deltaTime);
+        }
+        
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotatingSpeed = reverseRotating;
+        }
+        else
+        {
+            rotatingSpeed = forwardRotating;
         }
     }
 
     private void Move(KeyCode bindKey, int speedAndDirection)
     {
-        if (Input.GetKey(bindKey))
+        if (Input.GetKey(bindKey) && !isOnWater)
         {
             rb.AddForce(transform.forward * speedAndDirection * Time.deltaTime);
         }
-
-        if (Input.GetKey(KeyCode.DownArrow))
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.velocity = Vector3.zero;
+        if (collision.gameObject.name == "Water")
         {
-            rotatingSpeed = -70f;
+            isOnWater = true;
+            rb.velocity = Vector3.zero;
         }
-        else
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name != "Water")
         {
-            rotatingSpeed = 70f;
+            isOnWater = false;
         }
     }
 }
